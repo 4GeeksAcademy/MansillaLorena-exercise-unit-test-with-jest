49310e2eb9e33c5515eacc01c392d20d6091101c
MansillaLorena <lorenamansillag@gmail.com>
2024-09-28 00:44:30 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const sum = (a,b) => {
+    return a + b
+   
+    
+}
+
+console.log(sum(7,3))
+
+
+
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+// Declaramos una función con el nombre exacto "fromDollarToYen"
+const fromDollarToYen = function(valueInDollar) {
+    // Convertimos el valor a yenes japoneses
+    let valueInYen = (valueInDollar / oneEuroIs.USD) * oneEuroIs.JPY;
+    // Retornamos el valor en yenes
+    return valueInYen;
+}
+
+// Declaramos una función con el nombre exacto "fromYenToPound"
+const fromYenToPound = function(valueInYen) {
+    // Convertimos el valor a libras esterlinas
+    let valueInPound = (valueInYen / oneEuroIs.JPY) * oneEuroIs.GBP;
+    // Retornamos el valor en libras
+    return valueInPound;
+}
+
+// Exportamos las funciones
+module.exports = { sum,fromEuroToDollar,fromDollarToYen, fromYenToPound };
